a0125fc017fad7a4ac0d4102f79f0d775c56422f
Андрій Костюк <kostuk.andre.061@gmail.com>
2025-11-30 17:40:37 +0100

    Lab8: data pages

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import './styles/tw.css';
 import './styles/main.scss';
 
 import { initThemeToggle } from '/src/js/common/theme.js';
-import { initAbout } from '/src/js/app/aboutPage.js';
 
 initThemeToggle();
-initAbout().catch(error => {
-    console.error('Помилка ініціалізації About page:', error);
-});
+
+const page = document.body.dataset.page;
+
+const routes = {
+    about:    () => import('./js/app/aboutPage.js').then(m => m.initAbout()),
+};
+
+if (routes[page]) {
+    routes[page]().catch(err => {
+        console.error('Помилка ініціалізації сторінки:', err);
+    });
+}
