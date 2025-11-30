19849c4e7af788f11f6fe5ca2e07e995db0a4379
Андрій Костюк <kostuk.andre.061@gmail.com>
2025-11-30 17:31:56 +0100

    Lab8: employee fetching, cards

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import './styles/tw.css';
 import './styles/main.scss';
 
 import { initThemeToggle } from '/src/js/common/theme.js';
+import { initAbout } from '/src/js/app/aboutPage.js';
 
 initThemeToggle();
+initAbout().catch(error => {
+    console.error('Помилка ініціалізації About page:', error);
+});
